8bb8a4496a42c55296ebc3e69e45a6993fb5961b
yukoga <yukoga@gmail.com>
2016-04-05 01:46:33 +0900

    missing data.

--- chap3 preprocessing and transform/3-2 how to deal with missing data.R
+++ chap3 preprocessing and transform/3-2 how to deal with missing data.R
@@ -11,3 +11,15 @@ employee.IQ.JP$MCAR[c(1, 3, 10, 20)] <- NA
 employee.IQ.JP$MCAR.is.missing <- as.factor(as.integer(is.na(employee.IQ.JP$MCAR)))
 p <- ggplot(data = employee.IQ.JP, aes(x=IQ, y=JobPerformance, colour = MCAR.is.missing)) + geom_point(aes(shape = MCAR.is.missing), size = 5) + theme_bw() %+replace% theme(legend.position = "bottom")
 print(p)
+
+employee.IQ.JP$MAR <- employee.IQ.JP$JobPerformance
+employee.IQ.JP$MAR[1:5] <- NA
+employee.IQ.JP$MAR.is.missing <- as.factor(as.integer(is.na(employee.IQ.JP$MAR)))
+p2 <- ggplot(data = employee.IQ.JP, aes(x=IQ, y=JobPerformance, colour = MAR.is.missing)) + geom_point(aes(shape = MAR.is.missing), size = 5) + theme_bw() %+replace% theme(legend.position = "bottom")
+print(p2)
+
+employee.IQ.JP$MNAR <- employee.IQ.JP$JobPerformance
+employee.IQ.JP$MNAR[c(4:6, 10:11)] <- NA
+employee.IQ.JP$MNAR.is.missing <- as.factor(as.integer(is.na(employee.IQ.JP$MNAR)))
+p3 <- ggplot(data = employee.IQ.JP, aes(x=IQ, y=JobPerformance, colour = MNAR.is.missing)) + geom_point(aes(shape = MNAR.is.missing), size = 5) + theme_bw() %+replace% theme(legend.position = "bottom")
+print(p3)
